0aa37cdf084b883decbc91b8e5826d9f712294c5
FastBot <fastbot@example.com>
2026-05-28 17:57:49 +0200

    Fast Upload from Telegram Bot

---
+++ app/src/main/java/com/example/realapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.realapp;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView textView = new TextView(this);
+        textView.setText("Success! Your Bot works perfectly.");
+        setContentView(textView);
+    }
+}
